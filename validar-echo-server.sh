0c93d5c2261572873c938ee9b53aa2a08a77a5ce
francoalejo21 <falejo@fi.uba.ar>
2026-03-18 15:24:24 -0300

    new-script

---
+++ validar-echo-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MESSAGE="Hello world!"
+NETWORK="tp0_testing_net"
+SERVER_HOST="server"
+PORT="12345"
+
+RECIBIDO=$( echo $MESSAGE | docker run --rm -i --network $NETWORK busybox nc $SERVER_HOST $PORT)
+if [ "$RECIBIDO" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
+
+#Si queremos eliminar la imagen despues de validar que funciona el server:
+# docker rmi busybox
