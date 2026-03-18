817b9e5ffa2e3dda641ce91a61da55420a0840e8
francoalejo21 <falejo@fi.uba.ar>
2026-03-18 16:06:09 -0300

    fix typo error

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-MESSAGE="Hello world!"
+MESSAGE="Hello world"
 NETWORK="tp0_testing_net"
 SERVER_HOST="server"
 PORT="12345"
 
 RECIBIDO=$( echo $MESSAGE | docker run --rm -i --network $NETWORK busybox nc $SERVER_HOST $PORT)
-if [ "$RECIBIDO" == "$MESSAGE" ]; then
+if [ "$RECIBIDO" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
